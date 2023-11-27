d2bb2f5391d59081ec990f7a7753e821b482f5c3
Yusupov Ulugbek <yusupovulugbek73@gmail.com>
2023-11-27 10:45:08 +0500

    Add SV-Gate 2

--- dist/server.js
+++ dist/server.js
@@ -3,15 +3,15 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 const _app = require("./app");
-const _authroute = require("@routes/auth.route");
-const _customersroute = require("@routes/customers.route");
-const _cardsroute = require("@routes/cards.route");
+const _authroute = require("./routes/auth.route");
+const _customersroute = require("./routes/customers.route");
+const _cardsroute = require("./routes/cards.route");
 const _validateEnv = require("./utils/validateEnv");
-const _currencyroute = require("@routes/currency.route");
-const _categoryroute = require("@routes/category.route");
-const _merchantroute = require("@routes/merchant.route");
-const _serviceroute = require("@routes/service.route");
-const _transactionroute = require("@routes/transaction.route");
+const _currencyroute = require("./routes/currency.route");
+const _categoryroute = require("./routes/category.route");
+const _merchantroute = require("./routes/merchant.route");
+const _serviceroute = require("./routes/service.route");
+const _transactionroute = require("./routes/transaction.route");
 const _http = /*#__PURE__*/ _interop_require_default(require("http"));
 const _socket = /*#__PURE__*/ _interop_require_default(require("./socket/socket"));
 function _interop_require_default(obj) {
